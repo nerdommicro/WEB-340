f3c7aad0860ceb784721959fdfd3f107e4de6a52
Michelle Nesbitt <mn218@nerdymail.com>
2020-05-09 17:25:42 -0700

    3.2

--- week2/routes/nesbitt-assignment2.3.js
+++ week2/routes/nesbitt-assignment2.3.js
@@ -1,3 +1,18 @@
+/*
+======================================================
+; Title:  nesbitt-assignment
+; Author: Michelle Nesbitt
+; Date:   2020
+; Modified By: Michelle Nesbitt
+; Description: Demonstrates
+;=====================================================
+*/
+var assignment = "2.3"
+//Print my header to the screen
+const header = require('../Nesbitt-header.js');
+//Display my header with exercise number
+console.log(header.display("Michelle", "Nesbitt", "Exercise " + assignment));
+
 var express = require("express");
 var http = require("http");
 var app = express();
